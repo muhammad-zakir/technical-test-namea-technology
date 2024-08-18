fb0ba9c353d46b64eafae09d06ecde22c1e8e2be
Muhammad Zakir <hi@zakir.id>
2024-08-19 02:45:40 +0700

    Add the logic for validation card numbers

--- src/application/payments/commands/validate-card-number/command.ts
+++ src/application/payments/commands/validate-card-number/command.ts
@@ -1,3 +1,7 @@
+import { cardNetworks } from './constants/cardNetworks'
+import { validateCardNetwork } from './helpers/validateCardNetwork';
+import { cardNetwork } from './interfaces/cardNetwork';
+import { cardNetworkAccumulator } from './interfaces/cardNetworkAccumulator';
 import { validate } from './validator';
 
 export type validateCardNumberCommand = Readonly<{
@@ -8,11 +12,25 @@ export function makeValidateCardNumberCommand() {
   return async function validateCardNumberCommand(command: validateCardNumberCommand) {
     await validate(command);
 
-    
+    return cardNetworks.reduce(
+      (accumulator: cardNetworkAccumulator, cardNetwork: cardNetwork) => {
+        const isValidated = validateCardNetwork(cardNetwork, command.cardNumber);
+        if (isValidated) {
+          return {
+            cardNetwork: cardNetwork.name,
+            isValid: true
+          }
+        }
 
-    return {
-      cardNetwork: 'Nganu',
-      isValid: true
-    };
+        return {
+          cardNetwork: accumulator.cardNetwork,
+          isValid: accumulator.isValid,
+        };
+      },
+      {
+        cardNetwork: 'Unknown',
+        isValid: false,
+      } as cardNetworkAccumulator,
+    );
   };
 }
