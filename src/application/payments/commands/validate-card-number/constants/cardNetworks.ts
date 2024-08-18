605a83588955d3076e1b7036d4de13368cee623d
Muhammad Zakir <hi@zakir.id>
2024-08-19 02:45:13 +0700

    chore: reformat the constants

--- src/application/payments/commands/validate-card-number/constants/cardNetworks.ts
+++ src/application/payments/commands/validate-card-number/constants/cardNetworks.ts
@@ -1,73 +1,73 @@
 import { cardNetwork } from '../interfaces/cardNetwork';
 
-export const cardNetworks: cardNetwork = {
-  americanExpress: {
+export const cardNetworks: Array<cardNetwork> = [
+  {
     length: {
       minimum: 15,
-      maximum: 21
+      maximum: 21,
     },
     name: 'American Express',
     prefixes: [34, 37],
     rangedPrefixes: [
       {
         minimum: 88880,
-        maximum: 88889
-      }
-    ]
+        maximum: 88889,
+      },
+    ],
   },
-  dinersClub: {
+  {
     length: {
       minimum: 14,
-      maximum: 22
+      maximum: 22,
     },
     name: 'Diners Club',
     prefixes: [35],
     rangedPrefixes: [
       {
         minimum: 300,
-        maximum: 305
+        maximum: 305,
       },
       {
         minimum: 4567,
-        maximum: 4999
-      }
-    ]
+        maximum: 4999,
+      },
+    ],
   },
-  discover: {
+  {
     length: {
       minimum: 16,
-      maximum: 23
+      maximum: 23,
     },
     name: 'Discover',
     prefixes: [6011, 1234],
     rangedPrefixes: [
       {
         minimum: 622126,
-        maximum: 622925
-      }
-    ]
+        maximum: 622925,
+      },
+    ],
   },
-  masterCard: {
+  {
     length: {
       minimum: 16,
-      maximum: 20
+      maximum: 20,
     },
     name: 'Master Card',
     prefixes: [5],
     rangedPrefixes: [
       {
         minimum: 2221,
-        maximum: 2720
-      }
-    ]
+        maximum: 2720,
+      },
+    ],
   },
-  visa: {
+  {
     length: {
       minimum: 13,
-      maximum: 22
+      maximum: 22,
     },
     name: 'Visa',
     prefixes: [4],
-    rangedPrefixes: []
+    rangedPrefixes: [],
   },
-} as const;
+] as const;
