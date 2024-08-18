1080e4cc7c3dae9044c64e0577b566f0b53fc05d
Muhammad Zakir <hi@zakir.id>
2024-08-19 02:49:14 +0700

    patch exemplary functional test

---
+++ tests/api/client.ts
@@ -0,0 +1,16 @@
+import app from '@web/app';
+import Fastify from 'fastify';
+import supertest from 'supertest';
+
+export async function makeClient() {
+  const fastify = Fastify();
+
+  await app(fastify);
+
+  await fastify.ready();
+
+  return {
+    client: supertest(fastify.server),
+    seed,
+  };
+}
